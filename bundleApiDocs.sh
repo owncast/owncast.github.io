b2080d9f9cb5d6d109897a953b5358a56e683ddc
Gabe Kangas <gabek@real-ity.com>
2020-10-21 22:44:39 -0700

    Do not build API documentation

--- bundleApiDocs.sh
+++
@@ -1,3 +0,0 @@
-curl https://raw.githubusercontent.com/owncast/owncast/master/openapi.yaml > data/openapi.yaml
-mkdir static/api
-npx redoc-cli bundle data/openapi.yaml -o static/api/index.html --options '{"hideHostname": true, "pathInMiddlePanel": true}'
