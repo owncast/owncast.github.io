c4b12da611c697d45fa36db582eb6644b4a3a0c5
Jannik <jannik@outlook.com>
2021-01-30 23:03:55 +0100

    :lipstick: use prefers-color-scheme and remove toggle buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,3 @@
-document.getElementById('mode').addEventListener('click', () => {
-
-  document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-
-});
-
-if (localStorage.getItem('theme') === 'dark') {
-
-  document.body.classList.add('dark');
-
-}
-
 /* eslint-disable */
 var clipboard = new ClipboardJS('.btn-clipboard');
 
