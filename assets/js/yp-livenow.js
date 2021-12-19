691808dcebfd5b0fc306418faedb308123b9323d
Gabe Kangas <gabek@real-ity.com>
2021-12-19 13:55:58 -0800

    Filter out NSFW streams from home page

--- assets/js/yp-livenow.js
+++ assets/js/yp-livenow.js
@@ -10,41 +10,53 @@ async function getDirectory() {
     return;
   }
 
-  const response = await fetch('https://directory.owncast.online/api/active', {
+  const response = await fetch("https://directory.owncast.online/api/active", {
     mode: "cors", // same-origin, no-cors
   });
-  const streams = await response.json();
+  let streams = await response.json();
   if (!streams.length || streams.length < 2) {
     return;
   }
+
+  // Remove NSFW streams
+  streams = streams.filter((stream) => !stream.nsfw);
 
   // if full list is bigger than widow width, make list "flex-start", else center it.
   const reflex = () => {
     const list = document.getElementById("stream-list");
     if (listContainer.clientWidth > window.innerWidth) {
-      list.parentElement.style.justifyContent = 'flex-start';
+      list.parentElement.style.justifyContent = "flex-start";
     } else {
-      list.parentElement.style.justifyContent = 'center';
+      list.parentElement.style.justifyContent = "center";
     }
-  }
-  window.addEventListener('resize', reflex);
+  };
+  window.addEventListener("resize", reflex);
 
-  document.getElementById('container-live-now').style.display = "block";
+  document.getElementById("container-live-now").style.display = "block";
 
-  streams.forEach(function(stream) {
+  streams.forEach(function (stream) {
     const listItem = document.createElement("li");
-    listItem.className="stream-item";
-    
-    const itemMarkup = '<a href="'+ stream.url +'" target="_blank" rel="noopener noreferrer">' + 
-      '<img src="https://directory.owncast.online/api/image/thumb/' + stream.id +'" loading="lazy" aspect-ratio="16 / 9" alt="See '+ stream.name +'&apos; stream." />' +
-      '<span class="stream-name">'+ stream.name +'</span>' +
-    '</a>';
+    listItem.className = "stream-item";
+
+    const itemMarkup =
+      '<a href="' +
+      stream.url +
+      '" target="_blank" rel="noopener noreferrer">' +
+      '<img src="https://directory.owncast.online/api/image/thumb/' +
+      stream.id +
+      '" loading="lazy" aspect-ratio="16 / 9" alt="See ' +
+      stream.name +
+      '&apos; stream." />' +
+      '<span class="stream-name">' +
+      stream.name +
+      "</span>" +
+      "</a>";
 
     listItem.innerHTML = itemMarkup;
     listContainer.appendChild(listItem);
   });
   reflex();
 }
-(function(){
+(function () {
   getDirectory();
-}());
+})();
