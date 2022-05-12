ee74e3a2d1894f0d76691ad0df51643157408692
Gabe Kangas <gabek@real-ity.com>
2022-05-12 15:09:42 -0700

    Help move UI work forward by exposing our progress

---
+++ scripts/bundleComponentGuide.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+temp_dir=$(mktemp -d)
+web_dir=$(pwd)
+cd "$temp_dir" || exit
+git clone https://github.com/owncast/owncast.git
+cd owncast || exit
+git checkout webv2
+cd "$web_dir" || exit
+mv "$temp_dir"/owncast/docs/components static/
