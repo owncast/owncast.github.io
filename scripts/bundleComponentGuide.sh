cf5984225be61efef460a9a08f3bb2fe4c28d005
Gabe Kangas <gabek@real-ity.com>
2022-05-12 16:59:32 -0700

    Build storybook instead of just moving the built version

--- scripts/bundleComponentGuide.sh
+++ scripts/bundleComponentGuide.sh
@@ -4,7 +4,14 @@ web_dir=$(pwd)
 cd "$temp_dir" || exit
 git clone https://github.com/owncast/owncast.git
 cd owncast || exit
+
+# TODO: Delete this line when webv2 merges into develop
 git checkout webv2
+
+cd web || exit
+npm install --include-dev
+npm run build-storybook -- -o ../docs/components
+
 cd "$web_dir" || exit
 rm -rf static/components
 mv "$temp_dir"/owncast/docs/components static/
