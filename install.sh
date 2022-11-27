35c7e3d817321c9e802b17e148c3e706f4caa26f
gabek <gabek@users.noreply.github.com>
2022-11-27 00:55:23 +0000

    deploy: 578293f2af359a93edf23657b0a0ff86d5b62da5

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 # Install configuration
 if ! [ "${OWNCAST_VERSION:-}" ]; then
-  OWNCAST_VERSION="0.0.12"
+  OWNCAST_VERSION="0.0.13"
 fi
 
 if ! [ "${OWNCAST_INSTALL_DIRECTORY:-}" ]; then
