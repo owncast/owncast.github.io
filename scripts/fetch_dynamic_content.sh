a689ebdc2d011c46876afa4173512fa54993a0e2
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-23 22:31:36 +0100

    improve logs

--- scripts/fetch_dynamic_content.sh
+++ scripts/fetch_dynamic_content.sh
@@ -7,43 +7,46 @@ rm -f data/contributors*.json
 rm -f data/donors*.json
 
 json_response_validate() {
-    response_size=$(wc -c <"$1")
+    response_size=$(($(wc -c <"$1")))
+    printf "[got $response_size bytes] "
     if [ $response_size -le 5 ]; then
-        echo "got invalid response: \n$1"
+        printf '\nERROR: got invalid response: %s\n'"$1"
+        return 1
     fi
+     return 0
 }
 
-printf "Fetching Core Contributors..."
-curl -s -o data/contributors-core-raw.json https://api.github.com/repos/owncast/owncast/contributors?per_page=100
+printf "Fetching Core Contributors... "
+curl -f -s -o data/contributors-core-raw.json https://api.github.com/repos/owncast/owncast/contributors?per_page=100 || true
 json_response_validate data/contributors-core-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-core-raw.json | jq --slurp '.' > data/contributors-core.json
 printf "done!\n"
 
-printf "Fetching Admin Contributors..."
-curl -s -o data/contributors-admin-raw.json https://api.github.com/repos/owncast/owncast-admin/contributors?per_page=100
+printf "Fetching Admin Contributors... "
+curl -f -s -o data/contributors-admin-raw.json https://api.github.com/repos/owncast/owncast-admin/contributors?per_page=100 || true
 json_response_validate data/contributors-admin-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-admin-raw.json | jq --slurp '.' > data/contributors-admin.json
 printf "done!\n"
 
-printf "Fetching Home Page Contributors..."
-curl -s -o data/contributors-homepage-raw.json https://api.github.com/repos/owncast/owncast.github.io/contributors?per_page=100
+printf "Fetching Home Page Contributors... "
+curl -f -s -o data/contributors-homepage-raw.json https://api.github.com/repos/owncast/owncast.github.io/contributors?per_page=100 || true
 json_response_validate data/contributors-homepage-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-homepage-raw.json | jq --slurp '.' > data/contributors-homepage.json
 printf "done!\n"
     
-printf "Fetching donors..."
-curl -s -o data/donors-raw.json https://opencollective.com/owncast/members/all.json
+printf "Fetching donors... "
+curl -f -s -o data/donors-raw.json https://opencollective.com/owncast/members/all.json || true
 json_response_validate data/donors-raw.json
 jq 'map(.) | .[] | select(.role=="BACKER") | {login: .name, avatar_url: .image, html_url: .profile}' data/donors-raw.json | jq --slurp '.' > data/donors.json
 printf "done!\n"
 
-printf "Combining Contributors..."
+printf "Combining Contributors... "
 jq --argfile core data/contributors-core.json --argfile admin data/contributors-admin.json --argfile homepage data/contributors-homepage.json -n '$core + $admin + $homepage |unique_by(.login)' > data/contributors.json
 printf "done!\n"
 
-printf "Adding current development documentation..."
+printf "Adding current development documentation... "
 mkdir -p static/api/development
-curl -s -o static/api/development/index.html https://raw.githubusercontent.com/owncast/owncast/master/doc/api/index.html
+curl -f -s -o static/api/development/index.html https://raw.githubusercontent.com/owncast/owncast/master/doc/api/index.html || true
 printf "done!\n"
 
 # Clean-up Environment 
