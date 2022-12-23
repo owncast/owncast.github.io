a7a04a788bdfde91a83a5722cb54bc9f6159b7a3
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-24 00:00:37 +0100

    refactor scripts/fetch_dynamic_content.sh

--- scripts/fetch_dynamic_content.sh
+++ scripts/fetch_dynamic_content.sh
@@ -2,55 +2,65 @@
 set -e
 set -o pipefail
 
-# Clean-up Environment 
-rm -f data/contributors*.json
-rm -f data/donors*.json
+# TODO: check jq and curl exist
+# TODO: add json parser/validator
 
-json_response_validate() {
-    _json_error=0
-    _placeholder_file=${1/%.json/.old.json}
+use_placeholder_file() {
+    _placeholder_file=${1}.bk
+    echo "will use a placeholder file instead: $_placeholder_file"
+    mv "$_placeholder_file" "$1" || (echo "ERROR: placeholder file could not be found: $_placeholder_file"; exit 1)
+
+}
+
+response_file_validate() {
+    _response_file_error=0
 
     if [[ -f $1 ]]; then
-        _json_response=$(cat "$1")
-        _json_response_size=$((${#_json_response}))
+        _response_file_content=$(cat "$1")
+        _response_file_size=$((${#_response_file_content}))
 
-        printf '[got %i bytes] ' $_json_response_size
-        if [ $_json_response_size -le 5 ]; then # TODO: need a better test
-            printf '\nERROR: got invalid response: %s\n' "$_json_response"
-            _json_error=1
+        printf '[got %i bytes] ' $_response_file_size
+        if [ $_response_file_size -le 5 ]; then # TODO: need a better test
+            printf '\nERROR: got invalid response: %s\n' "$_response_file_content"
+            _response_file_error=1
         fi
     else
-        printf '\nERROR: could not open json file: %s\n' "$1"
-        _json_error=1
+        printf '\nERROR: could not open file: %s\n' "$1"
+        _response_file_error=1
     fi
 
-    if [[ _json_error -ne 0 ]]; then
-        echo "will use placeholder data instead: $_placeholder_file"
-        mv "$_placeholder_file" "$1"
+    if [[ _response_file_error -ne 0 ]]; then
+        use_placeholder_file "$1"
     fi
 }
 
+printf "Removing old content files... "
+rm -f data/contributors*.json
+rm -f data/donors*.json
+rm -f static/api/development/index.html
+printf "done!\n"
+
 printf "Fetching Core Contributors... "
 curl -f -s -o data/contributors-core-raw.json https://api.github.com/repos/owncast/owncast/contributors?per_page=100 || true
-json_response_validate data/contributors-core-raw.json
+response_file_validate data/contributors-core-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-core-raw.json | jq --slurp '.' > data/contributors-core.json
 printf "done!\n"
 
 printf "Fetching Admin Contributors... "
 curl -f -s -o data/contributors-admin-raw.json https://api.github.com/repos/owncast/owncast-admin/contributors?per_page=100 || true
-json_response_validate data/contributors-admin-raw.json
+response_file_validate data/contributors-admin-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-admin-raw.json | jq --slurp '.' > data/contributors-admin.json
 printf "done!\n"
 
 printf "Fetching Home Page Contributors... "
 curl -f -s -o data/contributors-homepage-raw.json https://api.github.com/repos/owncast/owncast.github.io/contributors?per_page=100 || true
-json_response_validate data/contributors-homepage-raw.json
+response_file_validate data/contributors-homepage-raw.json
 jq 'map(.) | .[] | {login: .login, avatar_url: .avatar_url, html_url: .html_url}' data/contributors-homepage-raw.json | jq --slurp '.' > data/contributors-homepage.json
 printf "done!\n"
     
 printf "Fetching donors... "
 curl -f -s -o data/donors-raw.json https://opencollective.com/owncast/members/all.json || true
-json_response_validate data/donors-raw.json
+response_file_validate data/donors-raw.json
 jq 'map(.) | .[] | select(.role=="BACKER") | {login: .name, avatar_url: .image, html_url: .profile}' data/donors-raw.json | jq --slurp '.' > data/donors.json
 printf "done!\n"
 
@@ -63,6 +73,7 @@ mkdir -p static/api/development
 curl -f -s -o static/api/development/index.html https://raw.githubusercontent.com/owncast/owncast/master/docs/api/index.html || true
 printf "done!\n"
 
-# Clean-up Environment 
+printf "Removing tmp files... "
 rm -f data/contributors-*.json
 rm -f data/donors-*.json
+printf "done!\n"
