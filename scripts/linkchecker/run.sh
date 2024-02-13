f2deb2de5c89ff56e4aa1fa48aeedf0f4e8d32a7
Gabe Kangas <gabek@real-ity.com>
2024-02-12 19:13:25 -0800

    Run lychee from cwd

--- scripts/linkchecker/run.sh
+++ scripts/linkchecker/run.sh
@@ -1,2 +1,2 @@
-lychee --github-token $GITHUB_TOKEN --exclude-path ../../content/releases -f json https://owncast.online/docs ../../content >links.json
+./lychee --github-token $GITHUB_TOKEN --exclude-path ../../content/releases -f json https://owncast.online/docs ../../content >links.json
 GITHUB_TOKEN=$GITHUB_TOKEN node main.js
