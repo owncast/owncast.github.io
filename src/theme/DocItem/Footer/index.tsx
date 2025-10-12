a7a8e4692a721d1464c7617623151212cda73095
Gabe Kangas <gabek@real-ity.com>
2025-10-11 21:35:42 -0700

    Re-add the doc contributor list back

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import OriginalFooter from "@theme-original/DocItem/Footer";
 import RelatedDocs from "@site/src/components/RelatedDocs";
+import ContributorsList from "@site/src/components/ContributorsList";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
 
 export default function FooterWrapper(
   props: React.ComponentProps<typeof OriginalFooter>
 ) {
+  const { metadata } = useDoc();
+
+  // Get the source file path relative to the repo root
+  const filePath = metadata.source;
+
   return (
     <>
       <OriginalFooter {...props} />
       <RelatedDocs title="Related" showScores={false} />
+      <ContributorsList filePath={filePath} />
     </>
   );
 }
