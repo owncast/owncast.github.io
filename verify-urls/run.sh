b8543091eeb93da01c3ddb9657cc78f297d872b7
Gabe Kangas <gabek@real-ity.com>
2025-12-09 15:04:07 -0800

    Add script to display broken links on the new site

---
+++ verify-urls/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python3 find_broken_urls.py --old old-sitemap.xml --new-base https://owncast-preview.surge.sh --old-base https://owncast.online
+
+# Display broken links table
+echo ""
+echo "=== Broken Links Report ==="
+echo ""
+printf "%-45s | %-6s | %s\n" "Old URL" "Status" "New URL"
+printf "%-45s-+-%-6s-+-%s\n" "---------------------------------------------" "------" "------------------------------------------------------------"
+
+while IFS="," read -r old_raw old_abs requested_url status final_url redirect_chain elapsed_ms error
+do
+  printf "%-45s | %-6s | %s\n" "$old_abs" "$status" "$requested_url"
+done < <(tail -n +2 broken.csv)
+
+echo ""
+echo "Total broken links: $(tail -n +2 broken.csv | wc -l)"
