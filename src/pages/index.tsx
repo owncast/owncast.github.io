e08401acff3e32757dbf56dd3ba7b5fb55ae0664
Gabe Kangas <gabek@real-ity.com>
2025-12-12 14:54:42 -0800

    Point get started button to Quickstart wizard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,7 @@ function HomepageHeader() {
             <div className={styles.buttons}>
               <Link
                 className="button button--secondary button--lg"
-                to="/docs/getting-started/install/"
+                to="/quickstart"
               >
                 Get started
               </Link>
